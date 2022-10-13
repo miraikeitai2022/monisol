d712db2b1e0a204a59e06562d1efcf9744bdf289
s2021035 <s2021035@kait.jp>
2022-10-13 23:08:06 +0900

    バックエンドとフロントエンドのフォルダの共通化

--- config/settings.py
+++ config/settings.py
@@ -74,6 +74,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
